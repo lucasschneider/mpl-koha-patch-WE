d37002507bde346ac4f516d6244133f26740fcef
Lucas Schneider <lrschneider94@gmail.com>
2017-06-03 15:25:56 -0500

    v18.0.4: Added patron message for lost item payments; added warning for replacement and other fees

--- build/content/scripts/notifyLostReplacementFees.js
+++ build/content/scripts/notifyLostReplacementFees.js
@@ -1,28 +1,28 @@
-﻿"use strict";
+"use strict";
 
 var fineLabels = document.getElementsByClassName('circ-hlt'),
-  usr  = document.getElementsByClassName('loggedinusername'),
-  label,
-  fineVal = "",
-  hasReplacementFee = false,
-  hasOtherFee = false,
-  mainform,
-  warning;
+    usr = document.getElementsByClassName('loggedinusername'),
+    label,
+    fineVal = "",
+    hasReplacementFee = false,
+    hasOtherFee = false,
+    mainform,
+    warning;
 
 function extractFineVal(textContent) {
   var fineStart = false,
-    fineVal = "";
+      fineVal = "";
 
   for (var j = 0; j < textContent.length; j++) {
     if (fineStart) {
       fineVal += textContent[j];
       if (textContent[j] === ".") {
-        fineVal += textContent[j+1];
-        fineVal += textContent[j+2];
+        fineVal += textContent[j + 1];
+        fineVal += textContent[j + 2];
         break;
       }
     }
-    if (textContent[j] ===  "$" && !fineStart) {
+    if (textContent[j] === "$" && !fineStart) {
       fineStart = true;
     }
   }
@@ -30,14 +30,13 @@ function extractFineVal(textContent) {
   return parseFloat(fineVal);
 }
 
-function postWarning() {
-}
+function postWarning() {}
 
 if (fineLabels && usr && fineLabels.length > 0 && /(mad|hpb|seq|smb|msb|pin|haw|lak|mea)/i.test(usr[0].textContent.trim())) {
-  for (var i  = 0; i < fineLabels.length; i++) {
+  for (var i = 0; i < fineLabels.length; i++) {
     label = fineLabels[i].textContent;
     if (label.includes("Fines:")) {
-      fineVal = extractFineVal(fineLabels[i].parentElement.textContent)
+      fineVal = extractFineVal(fineLabels[i].parentElement.textContent);
     }
     if (label.includes("Replacement fees")) {
       hasReplacementFee = true;
@@ -52,13 +51,13 @@ mainform = document.getElementById('mainform');
 
 if (mainform) {
   warning = document.createElement("div");
-  warning.setAttribute('style','border:2px solid #cc0000;border-radius:20px;padding:10px;color:#cc0000;text-align:justify;text-justify:inter-word;font-weight:bold;');
+  warning.setAttribute('style', 'border:2px solid #cc0000;border-radius:20px;padding:10px;color:#cc0000;text-align:justify;text-justify:inter-word;font-weight:bold;');
 
   if (fineVal < 20 && hasReplacementFee) {
     warning.textContent = "DO NOT check out materials per MPL policy due to replacement fees on this Patron's account. Materials may not be checked out if there are any amount of replacement or damaged item fees, even if they are less than $20.";
-    mainform.parentElement.insertBefore(warning,mainform);
+    mainform.parentElement.insertBefore(warning, mainform);
   } else if (fineVal < 20 && hasOtherFee) {
     warning.textContent = "This patron may have damaged item fees and therefore MAY NOT checkout materials per MPL policy. Please check the reason for the \"Other fees\" to see if they include damaged item or replacement fines.";
-    mainform.parentElement.insertBefore(warning,mainform);
+    mainform.parentElement.insertBefore(warning, mainform);
   }
 }
