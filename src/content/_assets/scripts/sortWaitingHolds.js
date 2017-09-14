8f2586e96e84de0fcef5ace92a0e4ffd5c9105e3
Lucas Schneider <lrschneider94@gmail.com>
2017-09-14 03:11:00 -0500

    v18.2.0 progress

--- src/content/_assets/scripts/sortWaitingHolds.js
+++ src/content/_assets/scripts/sortWaitingHolds.js
@@ -38,10 +38,10 @@ function getExpiration(date) {
 /** Define waiting hold object **/
 function WaitingHold(htmlTR) {
   var i = 0;
-  if (htmlTR.children/length > 5) i++
+  if (htmlTR.children.length > 5) i++
   this.html = htmlTR;
-  this.availableSince = htmlTR.children[0].textContent.trim();
-  this.expirationDate = getExpiration(new Date(htmlTR.children[0].textContent.trim()));
+  this.availableSince = htmlTR.children[0].textContent.trim().substring(0,11);
+  this.expirationDate = getExpiration(new Date(htmlTR.children[0].textContent.trim().substring(0,11)));
   this.title = htmlTR.children[i+1].children[0].textContent.replace(":","").trim();
   this.titleElt = htmlTR.children[i+1];
   this.type = "("+htmlTR.children[i+1].children[1].textContent.trim()+")";
@@ -62,13 +62,24 @@ function WaitingHold(htmlTR) {
   * limitDate: The date to filter
   **/
 function sortWaitingHolds(waitingHolds, sortCode, limitType, limitDate) {
+  numWaitingHolds = 0;
+  // Extract table data
+  if (holdTable && trArray) {
+    for (var i = 0; i < trArray.length; i++) {
+      waitingHolds.push(new WaitingHold(trArray[i]));
+      numWaitingHolds++;
+    }
+  }
+  
   switch(sortCode) {
     default:
       waitingHolds.sort(function(a, b) {
-        if (a.expirationDate < b.expirationDate) return -1;
-        else if (a.expirationDate > b.expirationDate) return 1;
-        else if (a.patronLast < b.patronLast) return -1;
+        if (a.patronLast < b.patronLast) return -1;
         else if (a.patronLast > b.patronLast) return 1;
+        else if (a.patronFirst < b.patronFirst) return -1;
+        else if (a.patronFirst > b.patronFirst) return 1;
+        else if (a.expirationDate < b.expirationDate) return -1;
+        else if (a.expirationDate > b.expirationDate) return 1;
         else if (a.title < b.title) return -1;
         else if (a.title > b.title) return 1;
         else return 0;
@@ -101,11 +112,7 @@ holdTableHead.children[0].insertBefore(expirationDateTH, holdTableHead.children[
 
 // Extract table data
 if (holdTable && trArray) {
-  for (var i = 0; i < trArray.length; i++) {
-    waitingHolds.push(new WaitingHold(trArray[i]));
-    numWaitingHolds++;
-  }
-  sortWaitingHolds(waitingHolds, "", "", "");
+  sortWaitingHolds(waitingHolds, "", "", "09/14/2017");
 }
 
 // Generate sort options
@@ -135,6 +142,12 @@ heldThrough.checked = true;
 dateFilter.id = "dateFilter";
 dateFilter.type = "text";
 dateFilter.placeholder = "MM/DD/YYYY";
+dateFilter.addEventListener('input', function (e) {
+  var df = document.getElementById('dateFilter');
+  if (df.value.length == 10) {
+    sortWaitingHolds(waitingHolds, "", "", df.value);
+  }
+});
 
 // Append them to their labels
 availableSinceLabel.setAttribute("for","availableSince");
@@ -148,23 +161,25 @@ heldThroughLabel.textContent = "Held Through: ";
 heldThroughLabel.appendChild(heldThrough);
 
 dateFilterLabel.setAttribute("for","dateFilter");
+dateFilterLabel.setAttribute("style","margin-left:2.5em;");
 dateFilterLabel.textContent = "Date: ";
 dateFilterLabel.appendChild(dateFilter);
 
 // Append lables to wrapper elements
-  
-/*<div id="sortWrapper" style="margin:1em;">
-  <span style="font-weight:bold;">Limit by Date: </span>
-  <span style="display:inline-block;vertical-align:middle;text-align:right;">
-    <label for="availableSince">
-	  Available Since: <input name="dateType" value="availableSince" id="availableSince" checked="true" type="radio">
-	</label>
-	<br>
-	<label for="heldThrough">
-	  Held Through: <input name="dateType" value="heldThrough" id="heldThrough" type="radio">
-	</label>
-  </span>
-  <label for="dateFilter">
-    Date: <input id="dateFilter" type="text">
-  </label>
-</div>*/
+sortWrapper.id = "sortWrapper";
+sortWrapper.setAttribute("style","margin:1em;");
+
+title.setAttribute("style","font-weight:bold;margin-right:2.5em;");
+title.textContent = "Limit by Date: ";
+
+dateTypeWrapper.setAttribute("style","display:inline-block;vertical-align:middle;text-align:right;");
+dateTypeWrapper.appendChild(availableSinceLabel);
+dateTypeWrapper.appendChild(br);
+dateTypeWrapper.appendChild(heldThroughLabel);
+
+// Compile sort wrapper
+sortWrapper.appendChild(title);
+sortWrapper.appendChild(dateTypeWrapper);
+sortWrapper.appendChild(dateFilterLabel);
+
+document.getElementsByClassName('yui-g')[0].insertBefore(sortWrapper,document.getElementsByClassName('yui-g')[0].children[1]);
