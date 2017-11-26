7431d4b6dba28c2743c1b19ca345180862be1cd9
Lucas Schneider <lrschneider94@gmail.com>
2017-11-26 16:21:52 -0600

    CD Holds progress

--- content/scripts/holdShelfActionCDs.js
+++ content/scripts/holdShelfActionCDs.js
@@ -1,7 +1,5 @@
 "use strict";
 
-
-// CDAMS, CDAMSID, CDJMS CDYMS
 var box = document.createElement('input'),
   boxLabel = document.createElement('label'),
   labelText = document.createElement('span'),
@@ -10,12 +8,43 @@ var box = document.createElement('input'),
   origTable = document.getElementById("mytab"),
   origHead,
   origBody,
-  CD;
+  cdArray = [],
+  otherArray = [];
+  
+  if (origTable) {
+    origHead = origTable.tHead.children[0];
+    origBody = origTable.tBodies[0];
+    
+    origHead.children[3].click();
+    
+    for (var i = 0; i < origHead.children.length; i++) {
+      if (origHead.children[i].textContent.trim() !== "Remove") {
+        origHead.children[i].addEventListener('click',function() {
+          if (!origHead.children[3].classList.contains("headerSortDown")) {
+            box.checked = false;
+          }
+        });
+      }
+    }
+  }
 
   function separateCDs() {
     if(this.checked) {
+      if (!origHead.children[3].classList.contains("headerSortDown")) {
+        origHead.children[3].click();
+      }
+
       if (origTable) {
-        origHead = origTable.tHead.children[0];
+        
+        for (var i = 0; i < origBody.length; i++) {
+          if (/^CD[AJY]MS|CDAMSID/.test(origBody.children[i].children[2].textContent.trim().substring(0,7))) {
+            cdArray.push(origBody.children[i]);
+          } else {
+            otherArray.push(origBody.children[i]);
+          }
+        }
+        
+        
       }
     }
   }
@@ -26,6 +55,7 @@ if (/\/cgi-bin\/koha\/reports\/holdsaction\.pl/.test(location.pathname)) {
   if (!tableNotLoaded) {
     box.type = "checkbox";
     box.checked = false;
+    box.style.verticalAlign = "middle";
     box.style.cursor = "pointer";
     box.style.margin = "0 1em 0 1.5em";
     boxLabel.style.cursor = "pointer";
