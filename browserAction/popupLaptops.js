df345e476d3df8c95fb9529724295ee00037c85d
Lucas Schneider <lschneider4@wisc.edu>
2019-02-23 16:50:31 -0600

    Dexie for laptop CKO; still need more library files

--- browserAction/popupLaptops.js
+++ browserAction/popupLaptops.js
@@ -3,7 +3,6 @@ var defaultMenu = document.getElementById('defaultMenu'),
   laptopFormOn = document.getElementById("laptopFormOn"),
   laptopFormOnSwitch = document.getElementById("laptopFormOnSwitch"),
   patronBarcode = document.getElementById("anyNumber"),
-  accessory = document.getElementById("accessories"),
   logLaptop = document.getElementById("logLaptop");
 
 
@@ -28,34 +27,34 @@ laptopFormOnSwitch.addEventListener('click', function() {
 updateContent();
 
 patronBarcode.addEventListener('input', validateInput);
-accessory.addEventListener('input', function() {
-  if (!/^[1-4]$/.test(this.value)) this.value = "";
-});
 
 logLaptop.innerText = "Log Entry";
 logLaptop.style.cursor = "pointer";
 logLaptop.addEventListener('click', logData);
 
-// https://doc.esdoc.org/github.com/rpl/idb-file-storage/class/src/idb-file-storage.js~IDBFileStorage.html#instance-method-get
-async function logData() {
-  const laptopLogs = await IDBFiles.getFileStorage({"name": "laptopLogs"});
+// https://dexie.org
+function logData() {
+  var db = new Dexie('laptopCheckoutTest');
+  db.version(1).stores({"issue":"++id,dateIn,barcode"});
 
-  const fileList = await laptopLogs.list();
-
-  const idbMutableFile = fileList.includes("test.txt") ?
-      await laptopLogs.get("test.txt") :
-      await laptopLogs.createMutableFile("test.txt");
-
-  const lockedFile = idbMutableFile.open("readwrite");
-
-  await lockedFile.append((new Date()).toUTCString() + "\n");
-
-  const metadata = await lockedFile.getMetadata();
-  const text = await lockedFile.readAsText(metadata.size);
-  console.log(text);
-
-  await lockedFile.close();
-  await idbMutableFile.persist();
+  db.open().then(() => {
+    return db.laptopCheckoutTest.add({
+      "dateIn": (new Date()).toString(),
+      "barcode": "290780XXXXXXXX"
+    })
+  }).then(() => {
+    return db.laptopCheckoutTest.toArray();
+  }).then(res => {
+    console.log(res);
+  }).catch (Dexie.MissingAPIError, function (e) {
+	log ("Couldn't find indexedDB API");
+}).catch ('SecurityError', function(e) {
+  log ("SecurityError - This browser doesn't like fiddling with indexedDB.");
+  log ("If using Safari, this is because jsfiddle runs its samples within an iframe");
+  log ("Go run some samples instead at: https://github.com/dfahlander/Dexie.js/wiki/Samples");
+}).catch (function (e) {
+	log (e);
+});
 }
 
 function validateInput() {
